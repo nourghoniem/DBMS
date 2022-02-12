8ae7acd67f186c7dc2c762fa9cfec1b2c5268233
nour <nour@192.168.1.14>
2022-02-12 23:12:26 +0200

    main list

--- DBMS/Home.sh
+++ DBMS/Home.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+PS3='Welcome, Please enter your choice: '
+options=("Create Database" "List Database" "Connect to Database" "Drop Database" "Quit")
+select opt in "${options[@]}"
+do
+  case $opt in
+         "Create Database")
+            echo "Creating Database"
+            break
+            ;;
+          "List Database")
+            echo "Listing Database"
+            break
+            ;;
+          "Connect to Database")
+            echo "Connecting to Database"
+            break
+            ;;
+          "Drop Database")
+            echo "Dropping Database"
+            break
+            ;;
+          "Quit")
+            break
+            ;;
+          *) echo "invalid option $REPLY";;
+    esac
+done
